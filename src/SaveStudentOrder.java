f6955c29446500d158d729fcbe99027bcf659a06
dmitri1 <dmitriirudko@icloud.com>
2022-03-21 01:43:33 +0300

    Initial commit

---
+++ src/SaveStudentOrder.java
@@ -0,0 +1,10 @@
+public class SaveStudentOrder {
+
+    public static void main(String[] args) {
+        saveStudentOrder();
+    }
+
+    static void saveStudentOrder() {
+        System.out.println("1");
+    }
+}
